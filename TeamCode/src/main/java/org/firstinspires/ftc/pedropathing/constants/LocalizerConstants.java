76829d2518ad456c58123750de733a0f71a161de
Tyler-Stocks <tylerjstocks@gmail.com>
2025-02-24 21:12:36 -0800

    Removed roadrunner from project and began setting up LocalizerConstants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/pedropathing/constants/LocalizerConstants.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.pedropathing.constants;
+
+import com.pedropathing.localization.constants.TwoWheelConstants;
+
+public class LocalizerConstants {
+    static {
+        TwoWheelConstants.forwardTicksToInches = 0.0;
+    }
+}
